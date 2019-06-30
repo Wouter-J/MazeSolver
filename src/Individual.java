2a54d68b0364aab4520e20c2c82b311a90df442f
RDAxRoadkill <wouter97@planet.nl>
2019-06-30 14:26:10 +0200

    Individual cleanup

--- src/Individual.java
+++ src/Individual.java
@@ -3,24 +3,19 @@ import java.util.List;
 
 public class Individual {
 
-    public List<Integer> moveList = new ArrayList<>();
-    public int[] moveBoi;
+    //public List<Integer> moveList = new ArrayList<>();
+    public int[] moveList;
     private Game game;
 
     public Individual(Game game){
-        this.moveList = moveList;
         this.game = game;
     }
 
     //Movement in the movelist is based on numbers.
     //1 = Up; 2 = Left; 3 = Right; 4 = Down
     public void Move(){
-        moveList.toArray();
-        for(int i = 0; i < moveBoi.length; i++){
-//            if(moveBoi[i] == 1){
-//
-//            }
-            switch(moveBoi[i]) {
+        for(int i = 0; i < moveList.length; i++){
+            switch(moveList[i]) {
                 case 1: {
                     System.out.println("Moving up!");
                     game.moveUp();
